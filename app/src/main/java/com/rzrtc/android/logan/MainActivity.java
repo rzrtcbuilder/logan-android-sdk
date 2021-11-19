9865796ded012e28f2044025ea18e6b436bd5207
lizejun <zejun.li@duobei.com>
2021-11-19 11:19:13 +0800

    1.0.0发布

--- app/src/main/java/com/rzrtc/android/logan/MainActivity.java
+++ app/src/main/java/com/rzrtc/android/logan/MainActivity.java
@@ -1,39 +1,79 @@
 package com.rzrtc.android.logan;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
-import com.rzrtc.android.logan.java.api.LoganException;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.rzrtc.android.logan.java.api.RzLogan;
 import com.rzrtc.android.logan.java.api.RzLoganConfig;
 import com.rzrtc.android.logan.java.callback.UploadCallback;
 import com.rzrtc.android.logan.java.impl.RzLoganFactory;
 
 public class MainActivity extends AppCompatActivity {
-
+    RzLogan loganInstance;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-            RzLogan loganInstance = RzLoganFactory.createRzLoganInstance(this, new RzLoganConfig.Builder()
-                    .setDeleteAfterUpload(true)
-                    .setFileDir(getExternalFilesDir("logan-dir").getAbsolutePath())
-                    .setUploadAddress("https://data-center-dev.duobeiyun.com/test/v1/logan")
-                    .setUploadInternal(1000 * 602)
-                    .setEncrypt(false)
-                    .setUpload(true)
-                    .build());
-            loganInstance.log("测试 upload");
-            loganInstance.uploadAllAsync(new UploadCallback() {
-                @Override
-                public void resultCallback(int errorCode) {
-                    Log.e("lzj", "resultCallback: errorCode" + errorCode );
-                    loganInstance.release();
-                }
-            });
+        findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loganInstance = RzLoganFactory.createRzLoganInstance(MainActivity.this, new RzLoganConfig.Builder()
+                        .setDeleteAfterUpload(true)
+                        .setFileDir(getExternalFilesDir("logan-dir").getAbsolutePath())
+                        .setUploadAddress("https://data-center-dev.duobeiyun.com/test/v1/logan")
+                        .setUploadInternal(1000 * 60)
+                        .setEncrypt(false)
+                        .setUpload(true)
+                        .build());
+            }
+        });
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thread(
+                        new Runnable() {
+                            @Override
+                            public void run() {
+                                for (int i = 0; i < 1000; i++) {
+                                    try {
+//                                        Thread.sleep(100);
+                                        loganInstance.log("测试 upload");
+
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            }
+                        }
+                ).start();
+            }
+        }); findViewById(R.id.button3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loganInstance.uploadAllAsync(new UploadCallback() {
+                    @Override
+                    public void resultCallback(int errorCode) {
+                        Log.e("lzj", "resultCallback: errorCode" + errorCode );
+//                        Toast.makeText(MainActivity.this,"errorCode " + errorCode,Toast.LENGTH_LONG ).show();
+                    }
+                });
+            }
+        }); findViewById(R.id.button4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loganInstance.release();
+            }
+        });
+
+
+
+
+
 
 
     }
